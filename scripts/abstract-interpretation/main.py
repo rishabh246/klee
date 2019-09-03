8dd976ac4a77e3368b3e25f5942c3c208fc4efee
Beyazit Yalcinkaya <beyazit@fit-imp.maas>
2019-09-03 14:00:22 +0000

    symbolic cache model and abstract interpreter are implemented

--- scripts/abstract-interpretation/main.py
+++ scripts/abstract-interpretation/main.py
@@ -13,17 +13,11 @@ ip_file = sys.argv[1]
 def main():
   
   ins_list = Parser.parse(ip_file)
-  print ins_list[0].IP
-  print ins_list[0].regs
-  print ins_list[0].category
-  print ins_list[0].opcode
-  print ins_list[0].disass
-  print ins_list[0].addresses
-  #init_state = ExecutionState(ins_list[0].regs, {})   #For some reason initial state is integers
-  #engine = ExecutionEngine(init_state)
-  #engine.execute_instruction(ins_list[0])
-  #print engine.state.regs
-  #print engine.state.mem
+  init_state = ExecutionState(ins_list[0].regs, {})   #For some reason initial state is integers
+  engine = ExecutionEngine(init_state)
+  engine.execute_instruction(ins_list[0])
+  print engine.state.regs
+  print engine.state.mem
 
 if __name__ == '__main__':
     main()
