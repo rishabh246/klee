ea8b171ae37ea188f0925b21ba59279d31a14bdb
Beyazit Yalcinkaya <beyazit@fit-imp.maas>
2019-09-03 13:54:34 +0000

    symbolic cache model and abstract interpreter are implemented

--- scripts/stateless_scripts/cache_setup.py
+++ scripts/stateless_scripts/cache_setup.py
@@ -25,17 +25,67 @@ symbol_re = re.compile("Irrelevant to Trace")
 symbol2_re = re.compile("Non-memory instruction")
 
 
+#### Find worst alignement of symbolic cache blocks ####
+f = open(ip_file, "r")
+ip_file_content = f.readlines()
+f.close()
+symbolic_memory_addresses = {}
+for line in ip_file_content:
+    text = line.rstrip()
+    if("SYMBOLIC" in text):
+        tmp = text.split(' ')
+        sym = tmp[0]
+        alignment = int(tmp[1])
+        offset = int(tmp[2])
+        if((sym, alignment) not in symbolic_memory_addresses.keys()):
+            symbolic_memory_addresses[(sym, alignment)] = []
+        symbolic_memory_addresses[(sym, alignment)].append(offset)
+symbolic_cache_blocks = []
+for s in symbolic_memory_addresses.keys():
+    best = (0, [])
+    furthest_address = max(symbolic_memory_addresses[s])
+    for i in range(s[1] - cache_block_size, 1, s[1]):
+        tmp = [(i, i + cache_block_size, 0)]
+        while(tmp[-1][1] < furthest_address):
+            tmp.append((tmp[-1][0] + cache_block_size, tmp[-1][1] + cache_block_size, (tmp[-1][2] + 1) % cache_block_size))
+        if all(any(map(lambda x: y in range(x[0], x[1]), tmp)) for y in symbolic_memory_addresses[s]):
+            miss_count = [any(map(lambda x: x in range(y[0], y[1]), symbolic_memory_addresses[s])) for y in tmp].count(True)
+            if(best[0] < miss_count):
+                best = (miss_count, tmp)
+    for bnds in best[1]:
+        symbolic_cache_blocks.append((s, bnds))
+########################################################
+
+
 def main():
-    global cache_contents
+    global cache_contents   
     global cache_ages
     with open(ip_file) as f:
-
         for line in f:
             text = line.rstrip()
             m1 = symbol_re.match(text)
             m2 = symbol2_re.match(text)
             if(m1):
                 age_cache_contents()
+            elif("SYMBOLIC" in text):
+                tmp = text.split(' ')
+                sym = tmp[0]
+                alignment = int(tmp[1])
+                offset = int(tmp[2])
+                used_symbolic_cache_block = None
+                for s in symbolic_cache_blocks:
+                    if sym == s[0][0] and alignment == s[0][1] and offset in range(s[1][0], s[1][1]):
+                        used_symbolic_cache_block = s
+                        break
+                if used_symbolic_cache_block:
+                    for set_no in range(len(cache_contents)):
+                        flag = True
+                        if used_symbolic_cache_block not in cache_contents[set_no]:
+                            flag = not any(type(i) == tuple and i[0] == used_symbolic_cache_block[0] and
+                                    i[1][2] != used_symbolic_cache_block[1][2] for i in cache_contents[set_no])
+                            cache_contents[set_no].append(used_symbolic_cache_block)
+                            cache_ages[set_no].append(None)
+                        update_ages(used_symbolic_cache_block, set_no, flag)
             elif(not m2):
                 addr = int(text, 16)
                 block_no = addr//cache_block_size  # Integer division
@@ -44,7 +94,6 @@ def main():
                     cache_contents[set_no].append(block_no)
                     cache_ages[set_no].append(None)
                 update_ages(block_no, set_no)
-
     print_cache_contents(op_file)
 
 
@@ -57,7 +106,7 @@ def age_cache_contents():
             cache_ages[x][y] = cache_ages[x][y]  # Don't clear cache.
 
 
-def update_ages(block_num, set_num):
+def update_ages(block_num, set_num, is_first_symbolic_or_concrete=True):
     global cache_contents
     global cache_ages
     index = cache_contents[set_num].index(block_num)
@@ -65,21 +114,43 @@ def update_ages(block_num, set_num):
     if (age == None):
         # This is for a new block, since all existing blocks must be aged
         age = set_associativity+1
+    if(not is_first_symbolic_or_concrete):
+        youngest_symbolic_cache_block_age = set_associativity + 1
+        for x in range(len(cache_ages[set_num])):
+            if(type(cache_contents[set_num][x]) == tuple and
+                    cache_contents[set_num][x][0] == block_num[0] and
+                    cache_contents[set_num][x][1][2] != block_num[1][2] and
+                    youngest_symbolic_cache_block_age > cache_ages[set_num][x]):
+                youngest_symbolic_cache_block_age = cache_ages[set_num][x]
     for x in range(len(cache_ages[set_num])):
         if(x == index):
             cache_ages[set_num][x] = 0
-        elif(not(x == index) and cache_ages[set_num][x] <= age):
+        elif(is_first_symbolic_or_concrete and 
+                cache_ages[set_num][x] <= age):
             cache_ages[set_num][x] += 1
-
+        elif(not is_first_symbolic_or_concrete and
+                cache_ages[set_num][x] < youngest_symbolic_cache_block_age):
+            cache_ages[set_num][x] += 1
 
 def print_cache_contents(op_file):
     global cache_contents
     global cache_ages
+    symbolic_cache_blocks_printed_overall = []
     with open(op_file, "w") as output:
         for x in range(len(cache_ages)):
+            symbolic_cache_blocks_printed_in_set = []
             for y in range(len(cache_ages[x])):
                 if(cache_ages[x][y] < set_associativity):
-                    output.write(str(hex(cache_contents[x][y])).upper()+"\n")
+                    try:
+                        output.write(str(hex(cache_contents[x][y])).upper()+"\n")
+                    except:
+                        if(cache_contents[x][y] not in symbolic_cache_blocks_printed_overall and 
+                            all(cache_contents[x][y] != i and (cache_contents[x][y][0] != i[0] or cache_contents[x][y][1][2] == i[1][2])
+                                for i in symbolic_cache_blocks_printed_in_set)):
+                            output.write(str(cache_contents[x][y][0][0]) + " " + str(cache_contents[x][y][1][0]) + " " + str(cache_contents[x][y][1][1]) + "\n")
+                            symbolic_cache_blocks_printed_overall.append(cache_contents[x][y])
+                            symbolic_cache_blocks_printed_in_set.append(cache_contents[x][y])
 
 
 main()
+
--- scripts/stateless_scripts/formal_cache.py
+++ scripts/stateless_scripts/formal_cache.py
@@ -1,5 +1,4 @@
 # Script to analyze a sequence of memory address using a simple 1 level, set associative LRU cache.
-#
 # $1: Input trace of memory addresses
 # $2: Output file, with each memory access tagged as a hit or a miss
 
@@ -26,6 +25,38 @@ symbol_re = re.compile("Irrelevant to Trace")
 symbol2_re = re.compile("Non-memory instruction")
 
 
+#### Find worst alignement of symbolic cache blocks ####
+f = open(ip_file, "r")
+ip_file_content = f.readlines()
+f.close()
+symbolic_memory_addresses = {}
+for line in ip_file_content:
+    text = line.rstrip()
+    if("SYMBOLIC" in text):
+        tmp = text.split(' ')
+        sym = tmp[0]
+        alignment = int(tmp[1])
+        offset = int(tmp[2])
+        if((sym, alignment) not in symbolic_memory_addresses.keys()):
+            symbolic_memory_addresses[(sym, alignment)] = []
+        symbolic_memory_addresses[(sym, alignment)].append(offset)
+symbolic_cache_blocks = []
+for s in symbolic_memory_addresses.keys():
+    best = (0, [])
+    furthest_address = max(symbolic_memory_addresses[s])
+    for i in range(s[1] - cache_block_size, 1, s[1]):
+        tmp = [(i, i + cache_block_size, 0)]
+        while(tmp[-1][1] < furthest_address):
+            tmp.append((tmp[-1][0] + cache_block_size, tmp[-1][1] + cache_block_size, (tmp[-1][2] + 1) % cache_block_size))
+        if all(any(map(lambda x: y in range(x[0], x[1]), tmp)) for y in symbolic_memory_addresses[s]):
+            miss_count = [any(map(lambda x: x in range(y[0], y[1]), symbolic_memory_addresses[s])) for y in tmp].count(True)
+            if(best[0] < miss_count):
+                best = (miss_count, tmp)
+    for bnds in best[1]:
+        symbolic_cache_blocks.append((s, bnds))
+########################################################
+
+
 def main():
     global cache_contents
     global cache_ages
@@ -34,7 +65,17 @@ def main():
         for block in common:
             block = block.strip()
             # In this scope we treat all block numbers as integers
-            common_lines.append(int(block, 16))
+            try:
+                common_lines.append(int(block, 16))
+            except:
+                tmp = block.split(' ')
+                sym = tmp[0]
+                lower = int(tmp[1])
+                upper = int(tmp[2])
+                for s in symbolic_cache_blocks:
+                    if sym == s[0][0] and lower == s[1][0] and upper == s[1][1]:
+                        common_lines.append(s)
+                        break
     with open(ip_file) as f:
         with open(op_file, "w") as output:
             for line in f:
@@ -46,6 +87,40 @@ def main():
                     output.write(text+"\n")
                 elif(m2):
                     output.write(text+"\n")
+                elif("SYMBOLIC" in text):
+                    tmp = text.split()
+                    sym = tmp[0]
+                    alignment = int(tmp[1])
+                    offset = int(tmp[2])
+                    used_symbolic_cache_block = None
+                    for s in symbolic_cache_blocks:
+                        if sym == s[0][0] and alignment == s[0][1] and offset in range(s[1][0], s[1][1]):
+                            used_symbolic_cache_block = s
+                            break
+                    hit = True
+                    if used_symbolic_cache_block:
+                        for set_no in range(len(cache_contents)):
+                            flag = True
+                            if used_symbolic_cache_block not in cache_contents[set_no] and used_symbolic_cache_block in common_lines:
+                                flag = not any(type(i) == tuple and i[0] == used_symbolic_cache_block[0] and
+                                        i[1][2] != used_symbolic_cache_block[1][2] for i in cache_contents[set_no])
+                                cache_contents[set_no].append(used_symbolic_cache_block)
+                                cache_ages[set_no].append(None)
+                            elif used_symbolic_cache_block in cache_contents[set_no]:
+                                index = cache_contents[set_no].index(used_symbolic_cache_block)
+                                if cache_ages[set_no][index] >= set_associativity:
+                                    hit = False
+                            else:
+                                flag = not any(type(i) == tuple and i[0] == used_symbolic_cache_block[0] and
+                                        i[1][2] != used_symbolic_cache_block[1][2] for i in cache_contents[set_no])
+                                cache_contents[set_no].append(used_symbolic_cache_block)
+                                cache_ages[set_no].append(None)
+                                hit = False
+                            update_ages(used_symbolic_cache_block, set_no, flag)
+                    if hit:
+                        output.write("Hit\n")
+                    else:
+                        output.write("Miss\n")
                 else:
                     addr = int(text, 16)
                     block_no = addr//cache_block_size  # Integer division
@@ -58,6 +133,8 @@ def main():
                         index = cache_contents[set_no].index(block_no)
                         if cache_ages[set_no][index] < set_associativity:
                             output.write("Hit\n")
+                        else:
+                            output.write("Miss\n")
                     else:
                         cache_contents[set_no].append(block_no)
                         cache_ages[set_no].append(None)
@@ -74,7 +151,7 @@ def age_cache_contents():
             cache_ages[x][y] = cache_ages[x][y]  # Don't clear cache.
 
 
-def update_ages(block_num, set_num):
+def update_ages(block_num, set_num, is_first_symbolic_or_concrete=True):
     global cache_contents
     global cache_ages
     index = cache_contents[set_num].index(block_num)
@@ -82,12 +159,25 @@ def update_ages(block_num, set_num):
     if (age == None):
         # This is for a new block, since all existing blocks must be aged
         age = set_associativity+1
+    if(not is_first_symbolic_or_concrete):
+        youngest_symbolic_cache_block_age = set_associativity + 1
+        for x in range(len(cache_ages[set_num])):
+            if(type(cache_contents[set_num][x]) == tuple and
+                    cache_contents[set_num][x][0] == block_num[0] and
+                    cache_contents[set_num][x][1][2] != block_num[1][2] and
+                    youngest_symbolic_cache_block_age > cache_ages[set_num][x]):
+                youngest_symbolic_cache_block_age = cache_ages[set_num][x]
     for x in range(len(cache_ages[set_num])):
         if(x == index):
             cache_ages[set_num][x] = 0
-        elif(not(x == index) and cache_ages[set_num][x] <= age):
+        elif(is_first_symbolic_or_concrete and 
+                cache_ages[set_num][x] <= age):
+            cache_ages[set_num][x] += 1
+        elif(not is_first_symbolic_or_concrete and
+                cache_ages[set_num][x] < youngest_symbolic_cache_block_age):
             cache_ages[set_num][x] += 1
 
 
 if __name__ == "__main__":
     main()
+
